6f881cc18f54b4cf485803e1081abd018a8706a1
Simon Stratmann <stratmann.simon@gmail.com>
2024-07-13 12:34:14 +0200

    Initial commit

---
+++ MoreShortcuts/Mod.cs
@@ -0,0 +1,25 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace MoreShortcuts {
+    public class Mod : IMod {
+        private static ILog log = LogManager.GetLogger($"{nameof(MoreShortcuts)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+
+        public void OnLoad(UpdateSystem updateSystem) {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+            updateSystem.UpdateAt<ReactToEvents>(SystemUpdatePhase.ToolUpdate);
+        }
+
+
+
+        public void OnDispose() {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
---
+++ MoreShortcuts/SetRoadModeStraight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Colossal.Logging;
+using Game;
+using Game.Prefabs;
+using Game.SceneFlow;
+using Game.Tools;
+using Game.UI.InGame;
+using Game.UI.Tooltip;
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace MoreShortcuts {
+    public partial class ReactToEvents : GameSystemBase {
+        
+        private NetToolSystem _netToolSystem;
+        
+        protected override void OnCreate() {
+            base.OnCreate();
+            _netToolSystem = World.GetExistingSystemManaged<NetToolSystem>();
+            var toolSystem = World.GetExistingSystemManaged<ToolSystem>();
+            toolSystem.EventPrefabChanged = (Action<PrefabBase>)Delegate.Combine( new Action<PrefabBase>(EventPrefabChanged),toolSystem.EventPrefabChanged);
+            toolSystem.EventToolChanged = (Action<ToolBaseSystem>)Delegate.Combine( new Action<ToolBaseSystem>(EventToolChanged),toolSystem.EventToolChanged);
+        }
+
+        private void EventToolChanged(ToolBaseSystem obj) {
+            if (obj.GetPrefab() is RoadPrefab) {
+                _netToolSystem.mode = NetToolSystem.Mode.Straight;
+            }
+        }
+        private void EventPrefabChanged(PrefabBase prefab) {
+            if (prefab is RoadPrefab) {
+                _netToolSystem.mode = NetToolSystem.Mode.Straight;
+            }
+        }
+        protected override void OnUpdate() {
+
+        }
+    }
+
+}
